cdf024ee522742e855a5cb5c834b7bff4de89750
Clyde Hack <clyde1976@gmail.com>
2023-03-08 20:11:12 -0600

    changed web history setting

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,20 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
 
 const router = createRouter({
-  history: createWebHashHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "home",
+      component: HomeView,
     },
     {
-      path: '/quiz/:name',
-      name: 'quiz',
-      component: () => import('../views/QuizView.vue')
-    }
-  ]
-})
+      path: "/quiz/:name",
+      name: "quiz",
+      component: () => import("../views/QuizView.vue"),
+    },
+  ],
+});
 
-export default router
+export default router;
